7a2763fd3836bdd72be378d55e5627ec6186e7b0
Ankit <ankitjaiswal0391@gmail.com>
2022-10-11 15:10:55 +0530

    binary sort

---
+++ SEARCHING_AND_SORTING/a.cpp
@@ -0,0 +1,17 @@
+int binary_search(int a[],int s,int n,int k){
+    // int n=n-1;
+    while(s<n){
+    int m=s+(n-s)/2;
+
+    if(k==a[m]){
+        return m;
+
+    }
+    else if(k<a[m]){
+        n=m-1;
+    }
+    else
+   { s=m+1;}
+    // m=(n-s)/2;
+ }
+ return -1;}
